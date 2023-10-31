8859af569ae9f1aa693741ca1a0cf3fe3d229d85
VitaliiKyrpach <vitalij.9f0x9@gmail.com>
2023-10-31 21:43:26 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,36 @@
+class Key {
+	private signature = Math.random();
+	getSignature() {
+		return this.signature;
+	}
+}
+
+class Person {
+	constructor(private key: Key) {}
+	getKey() {
+		return this.key;
+	}
+}
+
+abstract class House {
+	protected door: boolean = false;
+	protected key: Key;
+	protected tenants: Person[] = [];
+	constructor(key: Key) {}
+
+	abstract openDoor(key: Key): void;
+
+	comeIn(person: Person) {
+		if (this.door) this.tenants.push(person);
+	}
+}
+
+class MyHouse extends House {
+	openDoor(key: Key): void {
+		if (key.getSignature() === this.key.getSignature())
+			this.door = true;
+	}
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +39,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
